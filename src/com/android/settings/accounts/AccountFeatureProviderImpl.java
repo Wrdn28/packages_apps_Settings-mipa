0234ec56e087913dde0c1752a91551445e4b8f40
Wrdn28 <ifloraxd@gmail.com>
2024-05-17 23:15:07 +0000

    Revert "Show user's Google avatar on the homepage"
    This reverts commit ca301f9fc52e08fdabb97fc9ade6d19f27cfcd4c.

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -4,17 +4,14 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
 
-import com.android.settings.R;
-import com.android.settings.overlay.FeatureFactory;
-
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
     @Override
     public String getAccountType() {
-        return FeatureFactory.getAppContext().getString(R.string.account_type);
+        return "com.google";
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return AccountManager.get(context).getAccountsByType(getAccountType());
+        return AccountManager.get(context).getAccountsByType("com.google");
     }
 }
